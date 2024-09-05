d420715507e84a194695ee420e0b1092ec74e94b
Bernardo <bernardoduarte271@gmail.com>
2024-09-05 15:52:04 -0300

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #PROGRESS
 
 #Finished
-#Desktop: 4340 34HRC (2,5x10 - 5x10)
+#Desktop: 4340 34HRC (2,5x10 - 5x10), 4340 40HRC (2,5x10)
 #Notebook: 
 
 #Unfinished
-#Desktop: 4340 40HRC (2,5x10)
+#Desktop: 4340 40HRC (5x10)
 #Notebook: FI27 DISSEC67 (2,5x10)
